a9b92b51b928dfb19e83d6cc1bb98dd908d63de4
xuannga <nguyenx206@gmail.com>
2021-09-22 23:02:30 -0700

    updated db in server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -18,15 +18,13 @@ app.use(express.static("public"));
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/budget', {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
         useFindAndModify: false
     }
 );
 
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
